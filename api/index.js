bb79193993b5e3e5c88be6d2f7c6aaf5155f2e2c
shahid <shahidazhari313@gmail.com>
2024-02-23 00:29:29 +0530

    still env and db connected

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO);
+    console.log(`MongoDB Connected Succesfully`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log(`Server running on http://localhost:4000/`);
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='text-blue-600'>App</div>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
