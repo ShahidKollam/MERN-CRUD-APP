f3e45907b47a7631732b7845907d080aa31ebd13
shahid <shahidazhari313@gmail.com>
2024-02-24 06:18:17 +0530

    sign up backend connect

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,19 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
-  const { name, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password,10)
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   try {
-    const newUser = new User({ name, email, password: hashedPassword });
+    const newUser = new User({ username, email, password: hashedPassword });
     const saveUser = await newUser.save();
     console.log(saveUser);
-  
+
     res.status(201).json({ message: "User created" });
-    
   } catch (error) {
     // res.status(500).json(error.message)
-    next(error)
+    next(error);
   }
 };
