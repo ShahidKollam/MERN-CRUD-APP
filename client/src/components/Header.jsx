5ff6900fe695da9d2ef8ee51fb80a42456cc1c2e
shahid <shahidazhari313@gmail.com>
2024-02-25 06:52:38 +0530

    profile image set

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
- 
-export default function () {
+import { useSelector } from "react-redux";
+
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-300">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,10 +18,17 @@ export default function () {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-in">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
-          <li></li>
         </ul>
       </div>
     </div>
